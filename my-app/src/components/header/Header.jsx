8260b9e31323e801a0d5a93ef3d3a371479b2bab
voykinEgor <voikin.egor2@gmail.com>
2024-04-05 15:31:25 +0300

    First set

---
+++ my-app/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+    return(
+        <div>
+            <header>
+                <h1>
+                    Create App
+                </h1>
+                <p>description</p>
+            </header>
+            <nav>
+                <a href="#">One</a>
+                <a href="#">Two</a>
+                <a href="#">Three</a>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
