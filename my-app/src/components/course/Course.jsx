3bb90b75fb1f46e6818a4fe87fe40b59b87f7e8d
voykinEgor <voikin.egor2@gmail.com>
2024-04-19 16:05:58 +0300

    Add More

--- my-app/src/components/course/Course.jsx
+++ my-app/src/components/course/Course.jsx
@@ -1,13 +1,18 @@
 import styles from "./Course.module.scss";
 
-const Course = ({course}) =>{
+const Course = (props) =>{
+    const {course, deleteCourse} = props;
+
+    const hendleDelete = (id) => {
+        deleteCourse(id);
+    }
     return (
         <div className={styles.item}>
             <h3>{course.tittle}</h3>
                 <img src={course.img}></img>
                 <p>Description: {course.description}</p>
                 {course.hours > 130 ? <p className={styles.item__hard}>Hours: {course.hours}</p>: <p>Hours: {course.hours}</p>}
-            
+                <button onClick={()=>hendleDelete(course.id)}>Remove</button>            
         </div>
     )
 }
--- my-app/src/components/courseList/CourseList.jsx
+++ my-app/src/components/courseList/CourseList.jsx
@@ -1,18 +1,49 @@
 import Course from "../course/Course";
 import styles from "./CourseList.module.scss";
 import data from "../../data.json";
+import { useEffect, useState } from "react";
 
 
 const CourseList = () => {
     // console.log(data);
+    const [counter, setCounter] = useState(0);
+    
+    const handleCounter = () =>{
+        setCounter(counter + 1);
+    }
+
+    const handleVisible = () =>{
+        setVisibleCount(visibleCount + 1);
+    }
+
+    const [visibleCount, setVisibleCount] = useState(2);
+    const [courses, setCourses] = useState([]);
+    useEffect(()=>{
+        setCourses(data);
+    },[]) //заполнение списка только при загрузке сайта, если [] пустые, если [id], то меняется при изменении поля
+
+    const deleteCourse = (id) =>{
+        const updateCourses = courses.filter((item) => item.id != id);
+        setCourses(updateCourses);
+    }
+
+
+
+
     return(
-        <div className={styles.list}>
-            {data.map((item)=>
-                <div key={item.id}>
-                    <Course course={item}/>
-                </div>
-            )}
+        <div className={styles.mainblock}>
+            <div className={styles.list}>
+                {courses.slice(0,visibleCount).map((item)=>
+                    <div key={item.id}>
+                        <Course course={item} deleteCourse={deleteCourse}/>
+                    </div>
+                )}
+            </div>
+                    {visibleCount < courses.length && (
+                        <button onClick={handleVisible}>More {visibleCount}</button>
+                    )}
         </div>
+        
     )
 }
 
